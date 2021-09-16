97f6e0a568bb9bf654c17ced11985b62917d3872
mohammedowaisnoman <89965483+mohammedowaisnoman@users.noreply.github.com>
2021-09-17 00:59:57 +0530

    xyx

---
+++ evenNumOpt.js
@@ -0,0 +1,12 @@
+// const evenNum = (num) => {
+//   return num % 2 === 0 ? "This is Even" : "This is odd";
+// };
+
+// console.log(evenNum(6678));
+
+
+const evenNum = (num) => 
+     (num % 2 === 0 ? "This is Even" : "This is odd")
+  
+  
+  console.log(evenNum(6678));
